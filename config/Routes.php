febdb986e1680951188f7c13643944dcba7fb2c6
Milesdp <montesairsoft@gmail.com>
2018-01-07 23:22:47 +0200

    routes

--- config/Routes.php
+++ config/Routes.php
@@ -1,12 +1,17 @@
 <?php
 
 
+
+
      return array (
 
          'tasks/([a-z]+)/([0-9]+)' => 'tasks/$1/$2',
 
-         'tasks/([a-z])' => 'tasks/$1',
+         //'tasks/([a-z])' => 'tasks/$1',
+
+         'tasks/?([a-z])' => 'tasks/Index?$1',
 
          'tasks' => 'tasks/Index',
 
      );
+
